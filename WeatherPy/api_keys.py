67bf18e89fbae2addec0b00d5b31cfba451e32c3
anthonytranhoang <anthonytranhoang@gmail.com>
2024-07-01 13:02:30 +1000

    add file

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "fd2ab3e7076f8656b46c6f9613448340"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "2c30618124f6483c9d0012871ab52d0c"
